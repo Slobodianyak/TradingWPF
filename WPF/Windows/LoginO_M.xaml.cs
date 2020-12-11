251fd507730c3cf221674ce5db2f3e7442aff70d
Slobodianyak <63192578+Slobodianyak@users.noreply.github.com>
2020-12-11 11:06:08 +0200

    Add files via upload

---
+++ WPF/Windows/LoginO_M.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CourseProjectWPF.ViewModels;
+
+namespace CourseProjectWPF.Windows
+{
+    /// <summary>
+    /// Interaction logic for LoginO_M.xaml
+    /// </summary>
+    public partial class LoginO_M : Window
+    {
+        private readonly LoginO_MModel _loginViewModel;
+        public LoginO_M(LoginO_MModel vm)
+        {
+            _loginViewModel = vm;
+            DataContext = _loginViewModel;
+
+            InitializeComponent();
+        }
+
+        //public LoginO_M()
+        //{
+            
+        //}
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            //DialogResult = false;
+            this.Close();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (_loginViewModel.Login())
+            {
+                DialogResult = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials", "Error");
+            }
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+            if (this.DataContext != null)
+            {
+                ((dynamic)this.DataContext).Password = ((PasswordBox)sender).Password;
+            }
+        }
+    }
+}
