233d7f76bfb1dffcec31cfddf2f3562faa99f166
Slobodianyak <63192578+Slobodianyak@users.noreply.github.com>
2020-12-11 10:51:19 +0200

    Add files via upload

---
+++ BL/Interfaces/ICustomer.cs
@@ -0,0 +1,28 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Interfaces
+{
+    public interface ICustomer
+    {
+        CustomerDTO AddCustomer(CustomerDTO customer); 
+        void RemoveCustomer(int CustomerID);
+        CustomerDTO GetCustomer(int CustomerID);
+        CustomerDTO GetCustomerByLogin(string CustomerLogin);
+        bool Log(string Email, string Phone);
+
+
+        AddDTO AddPosition(AddDTO Add);
+        void DeletePosition(int OrderIDKEY);
+        List<AddDTO> SameOrderPositions(int OrderIDKEY);
+
+        List<ItemDTO> ShowItems();
+        ItemDTO GetItem(int ItemID);
+
+        OrderDTO AddOrder(OrderDTO order);
+    }
+}
---
+++ BL/Interfaces/IUser.cs
@@ -0,0 +1,35 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Interfaces
+{
+    public interface IUser
+    {
+        ItemDTO AddItem(ItemDTO item);
+        ItemDTO ChangeItem(ItemDTO item);
+        void ShowItemsSorted(int SortingParameter);
+        List<ItemDTO> ShowItems();
+        void RemoveItem(int ItemID);
+        ItemDTO GetItem(int ItemID);
+
+
+        bool Log(string login, string password);
+        Order_ManagerDTO AddOrder_Manager(Order_ManagerDTO Order_Manager); 
+        Order_ManagerDTO ChangeOrder_Manager(Order_ManagerDTO Order_Manager);
+        void RemoveOrder_Manager(int O_M_id);
+        Order_ManagerDTO GetOrder_Manager(int O_M_id);
+        Order_ManagerDTO GetOrder_ManagerByLogin(string Order_ManagerLogin);
+
+
+        OrderDTO GetOrder(int OrderID);
+        OrderDTO CompleteOrder(int OrderID);
+        void DeleteOrder(int OrderID);
+        List<OrderDTO> ShowOrders(int O_M_id);
+
+        void ShowCustomers();
+    }
+}
