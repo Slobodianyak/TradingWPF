87d78df3e0d8adf2963b735c833b793e84d1d906
Slobodianyak <63192578+Slobodianyak@users.noreply.github.com>
2020-12-11 10:53:18 +0200

    Add files via upload

---
+++ DAL_tests/AddDAL_test.cs
@@ -0,0 +1,52 @@
+﻿using DAL.Concrete;
+using DTO;
+using Microsoft.IdentityModel.Protocols;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace DAL_tests
+{
+    [TestFixture]
+    [Transaction(TransactionOption.RequiresNew), ComVisible(true)]
+    public class AddDAL_test
+    {
+        [Test]
+        public void CreateAddToOrderTest()
+        {
+            AddDAL dal = new AddDAL(ConfigurationManager.ConnectionStrings["Order_Manager"].ConnectionString);
+
+            var result = new AddDAL
+            {
+                Id_of_order = 5,
+                Id_of_item = 1,
+                Quantity = 28
+            };
+
+            result = dal.AddDAL(result);
+            Assert.IsTrue(result.Id_of_order == 5 && result.Id_of_item == 1, "AddToOrder was not created");
+
+        }
+
+
+        [Test]
+        public void GetAllFromSameOrderTest()
+        {
+            AddDAL dal = new AddDAL(ConfigurationManager.ConnectionStrings["Order_Manager"].ConnectionString);
+            var result = dal.FromSameOrder(6);
+
+            Assert.IsTrue(result.Count != 0, "no items shown"); ;
+
+        }
+
+
+    }
+}
---
+++ DAL_tests/ItemDAL_test.cs
@@ -0,0 +1,84 @@
+﻿using DAL.Concrete;
+using DTO;
+using Microsoft.IdentityModel.Protocols;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAL_tests
+{
+    [TestFixture]
+    [Transaction(TransactionOption.RequiresNew), ComVisible(true)]
+    public class ItemDAL_test
+    {
+        [Test]
+        public void CreateItemTest()
+        {
+            ItemDAL dal = new ItemDAL(ConfigurationManager.ConnectionStrings["Order_Manager"].ConnectionString);
+            var result = dal.CreateItem(new ItemDTO
+            {
+                Name = "Test Item",
+                Value = 157,
+            });
+            Assert.IsTrue(result.Item_id >= 0, "returned ID should be more than zero");
+
+        }
+
+
+        [Test]
+        public void GetItemByIDTest()
+        {
+            ItemDAL dal = new ItemDAL(ConfigurationManager.ConnectionStrings["Order_Manager"].ConnectionString);
+            var result = dal.GetItemById(1);
+            Assert.IsTrue(result.Item_id == 1, "returned ID does not match request");
+
+        }
+
+        [Test]
+        public void DeleteItemTest()
+        {
+            ItemDAL dal = new ItemDAL(ConfigurationManager.ConnectionStrings["Order_Manager"].ConnectionString);
+            dal.DeleteItem(12);
+
+            Assert.IsTrue(dal.GetItemById(12).Item_id != 12, "item still in db"); ;
+
+        }
+
+
+        [Test]
+        public void GetAllItemsTest()
+        {
+            ItemDAL dal = new ItemDAL(ConfigurationManager.ConnectionStrings["Order_Manager"].ConnectionString);
+            var result = dal.GetAllItems();
+
+            Assert.IsTrue(result.Count != 0, "no items shown"); ;
+
+        }
+
+        [Test]
+        public void UpdateItem()
+        {
+            ItemDAL dal = new ItemDAL(ConfigurationManager.ConnectionStrings["Order_Manager"].ConnectionString);
+
+            ItemDTO upd = dal.UpdateItem(new ItemDTO
+            {
+                Item_id = 4,
+                Value = 15,
+            });
+
+
+
+            Assert.IsTrue(upd.Value == 10, "Item was not updated");
+
+        }
+
+
+    }
+}
